6b5a17ec6e6e09cd585a62666e25538c70f1d6f0
tuppalwad <vtuppalwad@gmail.com>
2023-01-08 15:38:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Slider from "./components/slider";
+function App() {
+  return (
+    <div className="App">
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
